99e31c15e73bea3b1546ca00c8bb14b11603f308
Fitman22 <85381077+Fitman22@users.noreply.github.com>
2024-08-01 14:53:50 -0500

    navbar

--- Red-Artista/src/app/app.routes.ts
+++ Red-Artista/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-
+import { InicioComponent } from './inicio/inicio.component';
     export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
+    { path: 'Inicio', component: InicioComponent },
     ];
