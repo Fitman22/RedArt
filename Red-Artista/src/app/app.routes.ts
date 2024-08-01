2c05b0448263035637358272043020b16833262f
Fitman22 <85381077+Fitman22@users.noreply.github.com>
2024-08-01 15:14:20 -0500

    addFront

--- Red-Artista/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { InicioComponent } from './inicio/inicio.component';
-    export const routes: Routes = [
-    { path: '', redirectTo: '/login', pathMatch: 'full' },
-    { path: 'login', component: LoginComponent },
-    { path: 'Inicio', component: InicioComponent },
-    ];
